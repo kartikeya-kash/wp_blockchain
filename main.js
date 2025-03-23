dd9dd03c1f2de9a2b1398000644c7f7b132a6f82
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-23 15:09:50 +0530

    created block chain with some methods

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ class Block{
         return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
+
+class blockChain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()]; // array of block
+    }
+
+    createGenesisBlock(){
+        return new Block(0,'23/03/2025',"Genesis Block" , "0");
+    }
+
+    getLatestBlock(){ // to show the recently created block 
+return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock){ // to add a block to chain
+
+    }
+}
