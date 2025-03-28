a85bfe208022bf5c88ab6e95104846c8a11ce690
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-28 21:42:04 +0530

    changes

--- main.js
+++ main.js
@@ -6,16 +6,27 @@ class Block{
         this.data=data;
         this.previousHash=previousHash;
         this.hash=this.calculateHash();
+        this.nonce = 0;// a random value for proof of work
     }
 
     calculateHash(){ // to calculate the hash of the Block by taking the properties of the block
-        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.timestamp + this.previousHash +JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+while(this.hash.substring(0,difficulty) != Array(difficulty+1).join("0")){
+    this.nonce++;
+    this.hash = this.calculateHash();
+}
+
+console.log("Block Mined: "+ this.hash);
     }
 }
 
 class blockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()]; // array of block
+        this.difficulty = 2;
     }
 
     createGenesisBlock(){
@@ -28,7 +39,7 @@ return this.chain[this.chain.length-1];
 
     addBlock(newBlock){ // to add a block to chain
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash=newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty); // pass difficulty
     this.chain.push(newBlock);
     }
 isChainValid(){
@@ -52,11 +63,4 @@ isChainValid(){
 let kcoin = new blockChain();
 kcoin.addBlock(new Block(1, '1/02/2000', { amount: 4 }));
 kcoin.addBlock(new Block(2, '3/02/2000', { amount: 20 }));
-console.log("is chain valid ? " + kcoin.isChainValid());
-
-kcoin.chain[1].data = {amount:100};
-kcoin.chain[1].hash = kcoin.chain[1].calculateHash(); 
-
-console.log("is chain valid ? " + kcoin.isChainValid());
-
 console.log(JSON.stringify(kcoin,null,4));
