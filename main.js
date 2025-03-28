32969a4cf771c99fbd1dbf28dcd83257cd6d64d6
Kartikeya <kartikeya.anjul@gmail.com>
2025-03-28 21:23:14 +0530

    added function to check the valid chain

--- main.js
+++ main.js
@@ -31,6 +31,22 @@ return this.chain[this.chain.length-1];
     newBlock.hash=newBlock.calculateHash();
     this.chain.push(newBlock);
     }
+isChainValid(){
+    for(let i =1 ; i<this.chain.length;i++){
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i-1];
+
+        if(currentBlock.hash != currentBlock.calculateHash()){
+            return false;
+        }
+        if(currentBlock.previousHash != previousBlock.hash){
+            return false;
+        }
+    }
+
+    return true;
+}
+    
 }
 
 let kcoin = new blockChain();
